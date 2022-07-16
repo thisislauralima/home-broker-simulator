445525f21eb2d05a5fdd83cf8feb5ddaecdc40be
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 19:26:30 -0300

    feat: data mock file of stocks to invest

---
+++ src/data/stocksToInvest.js
@@ -0,0 +1,64 @@
+const stocksToInvest = [
+  {
+    id: 1,
+    name: 'Disney',
+    quantity: 50,
+    price: 120.23,
+  },
+  {
+    id: 2,
+    name: 'Amazon',
+    quantity: 37,
+    price: 100.20,
+  },
+  {
+    id: 3,
+    name: 'Magalu',
+    quantity: 98,
+    price: 50.10,
+  },
+  {
+    id: 4,
+    name: 'XP Inc',
+    quantity: 23,
+    price: 90.07,
+  },
+  {
+    id: 5,
+    name: 'Rico',
+    quantity: 87,
+    price: 12.65,
+  },
+  {
+    id: 6,
+    name: 'Netflix',
+    quantity: 55,
+    price: 32.17,
+  },
+  {
+    id: 7,
+    name: 'Americanas',
+    quantity: 21,
+    price: 4.21,
+  },
+  {
+    id: 8,
+    name: 'Mercado Livre',
+    quantity: 67,
+    price: 12.21,
+  },
+  {
+    id: 9,
+    name: 'IFood',
+    quantity: 52,
+    price: 8.23,
+  },
+  {
+    id: 10,
+    name: 'Apple',
+    quantity: 45,
+    price: 13.07,
+  },
+];
+
+export default stocksToInvest;
