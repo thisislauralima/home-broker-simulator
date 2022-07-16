62b9528436650429fd24ba2aef99b21a494e63b7
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 18:52:20 -0300

    feat: index.js with all pages

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Login from './Login/Login';
+import StockList from './StockList/StockList';
+import NotFound from './NotFound/NotFound';
+
+const pages = {
+  Login,
+  StockList,
+  NotFound,
+};
+
+export default pages;
