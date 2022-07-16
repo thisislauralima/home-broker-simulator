fe467139f2fe0885a4dd7ada7c1f31f846edc41c
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 19:27:08 -0300

    refactor: make StocksToInvest table using map

--- src/components/StocksToInvest/StocksToInvest.jsx
+++ src/components/StocksToInvest/StocksToInvest.jsx
@@ -1,47 +1,33 @@
 import React from 'react';
+import stocksToInvest from '../../data/stocksToInvest';
+import tableInfo from '../../data/tableHeader';
 
 export default function StocksToInvest() {
   return (
-    <table>
-      <caption>Ações para investir:</caption>
-      <thead>
-        <tr>
-          <th>Ação</th>
-          <th>Quantidade</th>
-          <th>Valor(R$)</th>
-          <th>Negociar</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {/* td - table data (célula) */}
-          <td>XPTO</td>
-          <td>1</td>
-          <td>350,00</td>
-          <td>
-            <button type="button">C</button>
-            <button type="button">V</button>
-          </td>
-        </tr>
-        <tr>
-          <td>XPTO</td>
-          <td>1</td>
-          <td>150,00</td>
-          <td>
-            <button type="button">C</button>
-            <button type="button">V</button>
-          </td>
-        </tr>
-        <tr>
-          <td>XPTO</td>
-          <td>1</td>
-          <td>450,00</td>
-          <td>
-            <button type="button">C</button>
-            <button type="button">V</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <main>
+      <table>
+        <caption>Ações para investir:</caption>
+        <thead>
+          {
+            tableInfo.map((info) => (
+              <th>{ info }</th>
+            ))
+          }
+        </thead>
+        {
+        stocksToInvest.map((stock) => (
+          <tbody>
+            <td>{ stock.name }</td>
+            <td>{ stock.quantity }</td>
+            <td>{ stock.price }</td>
+            <td>
+              <button type="button">C</button>
+              <button type="button">V</button>
+            </td>
+          </tbody>
+        ))
+      }
+      </table>
+    </main>
   );
 }
