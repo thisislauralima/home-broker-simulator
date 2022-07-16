86ce3e25cfc54d30230793fcaacce8e1ce751e08
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 18:59:27 -0300

    feat: create Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Login() {
+  const history = useHistory();
+  const goToStockListPage = () => {
+    history.push('/stock/list');
+  };
+
+  return (
+    <form>
+      <input type="text" placeholder="E-mail" />
+      <input type="password" placeholder="Senha" />
+      <button type="button" onClick={ goToStockListPage }>Acessar</button>
+    </form>
+  );
+}
