6b9da455fdd8ecaaf4b1370a4bb9272674252ec0
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 22:43:03 -0300

    feat: add new pages to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import Login from './Login/Login';
 import StockList from './StockList/StockList';
+import StockBuyOrSale from './StockBuyOrSale/StockBuyOrSale';
+import DepositOrWithdraw from './DepositOrWithdraw/DepositOrWithdraw';
 import NotFound from './NotFound/NotFound';
 
 const pages = {
   Login,
   StockList,
   NotFound,
+  StockBuyOrSale,
+  DepositOrWithdraw,
 };
 
 export default pages;
