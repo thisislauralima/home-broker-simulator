90d09fed9bd8d9ebc20f8a8a964a838aa82b6f1f
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 22:39:38 -0300

    refactor: fixes html  semantics and adds history functionality

--- src/components/StocksToInvest/StocksToInvest.jsx
+++ src/components/StocksToInvest/StocksToInvest.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import stocksToInvest from '../../data/stocksToInvest';
 import tableInfo from '../../data/tableHeader';
 
 export default function StocksToInvest() {
+  const history = useHistory();
+
+  const goToStockOrBuySalePage = () => {
+    history.push('acoes/transacoes');
+  };
+
   return (
     <main>
       <table>
         <caption>Ações para investir:</caption>
         <thead>
-          {
+          <tr>
+            {
             tableInfo.map((info) => (
-              <th>{ info }</th>
+              <th key={ info }>{ info }</th>
+            ))
+              }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            stocksToInvest.map((stock) => (
+              <tr key={ stock.id }>
+                <td>{ stock.name }</td>
+                <td>{ stock.quantity }</td>
+                <td>{ stock.price }</td>
+                <td>
+                  <button onClick={ goToStockOrBuySalePage } type="button">C</button>
+                  <button type="button">V</button>
+                </td>
+              </tr>
             ))
           }
-        </thead>
-        {
-        stocksToInvest.map((stock) => (
-          <tbody>
-            <td>{ stock.name }</td>
-            <td>{ stock.quantity }</td>
-            <td>{ stock.price }</td>
-            <td>
-              <button type="button">C</button>
-              <button type="button">V</button>
-            </td>
-          </tbody>
-        ))
-      }
+        </tbody>
       </table>
     </main>
   );
