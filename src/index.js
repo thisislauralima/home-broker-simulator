dd533c4854dccd3d553a2936886cde3c325ea51b
Laura Lima <lauraolima2000@gmail.com>
2022-07-16 22:42:40 -0300

    fix: fix StocksProvider import

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StocksProvider from './context/StockProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StocksProvider>
+        <App />
+      </StocksProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
