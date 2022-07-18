7c26f3cfd955fb733716c3f5936a5b2887d91210
Laura Lima <lauraolima2000@gmail.com>
2022-07-17 22:51:34 -0300

    feat: add link to personal stocks on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>Usuário XPTO</header>
+    <header>
+      <p>Usuário XPTO</p>
+      <Link to="acoes/cliente">Suas Ações</Link>
+    </header>
   );
 }
