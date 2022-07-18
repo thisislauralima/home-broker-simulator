0a1dbb2ff4d86bd0581a485835aadff4293a676f
Laura Lima <lauraolima2000@gmail.com>
2022-07-17 22:51:07 -0300

    feat: add new routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import pages from './pages';
 
 function App() {
+  const [renderLogin, setRenderLogin] = useState(false);
+  useEffect(() => {
+    setRenderLogin(true);
+  });
+
   return (
     <Switch>
       <Route exact path="/login" component={ pages.Login } />
+      <Route
+        exact
+        path="/"
+        render={ () => (
+          renderLogin && <Redirect to="/login" />
+        ) }
+      />
       <Route exact path="/acoes" component={ pages.StockList } />
-      <Route exact path="/acoes/transacoes" component={ pages.StockBuyOrSale } />
+      <Route exact path="acoes/cliente" component={ pages.PersonalStocks } />
       <Route exact path="/acoes/deposito/retirada" component={ pages.DepositOrWithdraw } />
       <Route exact path="*" component={ pages.NotFound } />
     </Switch>
