3c6afad09c9224fc35034e8ea94acb6929fc267b
Laura Lima <lauraolima2000@gmail.com>
2022-07-17 22:54:24 -0300

    refactor: rename data file with all stocks

---
+++ src/data/allStocks.js
@@ -0,0 +1,76 @@
+import logo from '../images/disney-logo.jpeg';
+
+const allStocks = [
+  {
+    id: 1,
+    name: 'Disney',
+    quantity: 50,
+    price: 120.23,
+    image: logo,
+  },
+  {
+    id: 2,
+    name: 'Amazon',
+    quantity: 37,
+    price: 100.20,
+    image: logo,
+  },
+  {
+    id: 3,
+    name: 'Magalu',
+    quantity: 98,
+    price: 50.10,
+    image: logo,
+  },
+  {
+    id: 4,
+    name: 'XP Inc',
+    quantity: 23,
+    price: 90.07,
+    image: logo,
+  },
+  {
+    id: 5,
+    name: 'Rico',
+    quantity: 87,
+    price: 12.65,
+    image: logo,
+  },
+  {
+    id: 6,
+    name: 'Netflix',
+    quantity: 55,
+    price: 32.17,
+    image: logo,
+  },
+  {
+    id: 7,
+    name: 'Americanas',
+    quantity: 21,
+    price: 4.21,
+    image: logo,
+  },
+  {
+    id: 8,
+    name: 'Mercado Livre',
+    quantity: 67,
+    price: 12.21,
+    image: logo,
+  },
+  {
+    id: 9,
+    name: 'IFood',
+    quantity: 52,
+    price: 8.23,
+    image: logo,
+  },
+  {
+    id: 10,
+    name: 'Apple',
+    quantity: 45,
+    price: 13.07,
+    image: logo,
+  },
+];
+
+export default allStocks;
