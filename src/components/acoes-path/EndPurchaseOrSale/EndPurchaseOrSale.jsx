be67cb46df1a01b2a816c7df996de1c755b759ac
Laura Lima <lauraolima2000@gmail.com>
2022-07-21 01:13:24 -0300

    feat: create EndPurchaseOrSale component
    makes possible to end stock purchase or sale

---
+++ src/components/acoes-path/EndPurchaseOrSale/EndPurchaseOrSale.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function EndPurchaseOrSale() {
+  return (
+    <div>
+      <h3>FINALIZAR OPERAÇÃO</h3>
+      <p>Empresa/ Código</p>
+      <p>Quantidade</p>
+      <p>Valor investimento</p>
+      <button type="button">ENVIAR</button>
+    </div>
+  );
+}
