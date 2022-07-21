4f722b978176289dbc86f7036c013a0753398fc0
Laura Lima <lauraolima2000@gmail.com>
2022-07-21 18:20:29 -0300

    fix: html error: reset input number from null to undefined
    change made in LeftSideStockMenuForm component

--- src/components/acoes-path/NegotiatedStocksMenu/StockMenuForm/LeftSideStockMenuForm/LeftSideStockMenuForm.jsx
+++ src/components/acoes-path/NegotiatedStocksMenu/StockMenuForm/LeftSideStockMenuForm/LeftSideStockMenuForm.jsx
@@ -25,7 +25,7 @@ export default function LeftSideStockMenuForm() {
   };
 
   const stockFinalPrice = ({ target: { value } }) => {
-    setRestartStockMenuInputs({ quantity: null });
+    setRestartStockMenuInputs({ quantity: undefined });
     setStockCode([]);
     const stockPrice = allStocks.filter(
       (stock) => stock.id === stockInfo[0].id,
@@ -37,7 +37,7 @@ export default function LeftSideStockMenuForm() {
   };
 
   const savePaidPrice = ({ target: { value } }) => {
-    setRestartStockMenuInputs({ price: null });
+    setRestartStockMenuInputs({ price: undefined });
     setPaidPriceForStock(value);
   };
 
