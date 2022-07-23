47272343ba7a9577b8441c2ff22178a24accd55c
Laura Lima <lauraolima2000@gmail.com>
2022-07-23 17:35:03 -0300

    fix: don't access .email on getItem on Header component
    by accessing .email an error was thrown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
 import './header.css';
 import stockContext from '../../context/stockContext';
 
 export default function Header() {
-  const { setErrorMessage, userEmail } = useContext(stockContext);
+  const { setErrorMessage } = useContext(stockContext);
   const history = useHistory();
 
   useEffect(() => {
-    if (!userEmail) {
+    if (!JSON.parse(localStorage.getItem('userInfo'))) {
       history.push('/login');
     }
   });
@@ -22,7 +24,10 @@ export default function Header() {
       <Link onClick={ cleanErrorMessage } to="/acoes/">Cotações</Link>
       <Link onClick={ cleanErrorMessage } to="/acoes/carteira">Carteira</Link>
       <Link onClick={ cleanErrorMessage } to="/acoes/transacoes">Depósito ou retirada</Link>
-      <p>{ userEmail }</p>
+      {
+        JSON.parse(localStorage.getItem('userInfo'))
+          ? <p>{JSON.parse(localStorage.getItem('userInfo')).email }</p> : history.push('/login')
+      }
     </header>
   );
 }
