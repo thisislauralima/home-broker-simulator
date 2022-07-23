51b853d0b955ec6f9d7da38024557675a2446f0c
Laura Lima <lauraolima2000@gmail.com>
2022-07-23 00:35:37 -0300

    feat: error validation during stock purchase

--- src/components/acoes-path/NegotiatedStocksMenu/StockMenuForm/EndOperationBtn/EndOperationBtn.jsx
+++ src/components/acoes-path/NegotiatedStocksMenu/StockMenuForm/EndOperationBtn/EndOperationBtn.jsx
@@ -5,18 +5,57 @@ import './endOperationBtn.css';
 export default function EndOperationBtn() {
   const {
     stockFinalPriceDecimal,
-    setIsPriceAndQuantityMissing,
-    paidPriceForStock,
-    setIsPurchaseOrSaleDone,
+    inputValueQuantityStock,
+    inputValueStockCode,
+    accountBalance,
+    stockInfo,
+    setErrorMessage,
+    setIsEndPurchaseOrSaleRendered,
+    setAccountBalance,
   } = useContext(stockContext);
 
+  const validateStockCodeInput = () => {
+    if (!inputValueStockCode) {
+      setErrorMessage('Insira o código do ativo.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
+  const validateStockQuantity = () => {
+    if (inputValueQuantityStock === 0) {
+      setErrorMessage('Selecione a quantidade do lote.');
+      return false;
+    } if (inputValueQuantityStock % 100 !== 0) {
+      setErrorMessage('A quantidade de ação não é múltipla de lote.');
+      return false;
+    } if (inputValueQuantityStock > stockInfo[0].quantity) {
+      setErrorMessage('A quantidade do lote inserida não pode ser superior a disponível.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
+  const updateAccountBalance = () => {
+    const accountDiscount = accountBalance - stockFinalPriceDecimal;
+    setAccountBalance(accountDiscount);
+  };
+
+  const validateAccountBalance = () => {
+    if (stockFinalPriceDecimal > accountBalance) {
+      setErrorMessage('O saldo da conta é inferior a quantidade solicitada.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
   const endOperation = () => {
-    if (!stockFinalPriceDecimal || !paidPriceForStock) {
-      setIsPriceAndQuantityMissing(true);
-      setIsPurchaseOrSaleDone(true);
-    } else {
-      setIsPriceAndQuantityMissing(false);
-      setIsPurchaseOrSaleDone(true);
+    if (validateStockCodeInput() === 'tudo certo'
+      && validateStockQuantity() === 'tudo certo'
+      && validateAccountBalance() === 'tudo certo') {
+      setErrorMessage('');
+      setIsEndPurchaseOrSaleRendered(true);
+      updateAccountBalance();
     }
   };
 
