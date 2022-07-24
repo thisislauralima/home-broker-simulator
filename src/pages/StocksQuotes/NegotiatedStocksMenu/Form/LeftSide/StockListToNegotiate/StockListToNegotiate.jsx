6a4dc323ac6ff9f5da7976b5ee9014c301d592b5
Laura Lima <lauraolima2000@gmail.com>
2022-07-24 09:00:49 -0300

    feat: style short menu with stocks to invest

--- src/pages/StocksQuotes/NegotiatedStocksMenu/Form/LeftSide/StockListToNegotiate/StockListToNegotiate.jsx
+++ src/pages/StocksQuotes/NegotiatedStocksMenu/Form/LeftSide/StockListToNegotiate/StockListToNegotiate.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import allStocks from '../../../../../../data/allStocks';
 import stockContext from '../../../../../../context/stockContext';
-import './stockListToNegotiate.css';
+// import './stockListToNegotiate.css';
 
 export default function StockListToNegotiate() {
   const {
@@ -12,7 +12,7 @@ export default function StockListToNegotiate() {
     setStockFinalPriceDecimal,
     inputValueQuantityStock,
     setInputValueQuantityStock,
-    btnColor,
+    // btnColor,
     isStocksCodeRendered,
     stockInfo,
     setInputValueStockCode,
@@ -48,26 +48,42 @@ export default function StockListToNegotiate() {
   };
 
   return (
-    <div>
+    <>
+      <div className="text-boleta-form-gray">Cd.Ativo</div>
       <input
         value={ inputValueStockCode }
-        className={ `${btnColor} short-menu-input` }
+        className="text-white w-40 border-none rounded-md short-menu-input bg-boleta-input-color"
         type="text"
         onInput={ (e) => listStocksCodes(e) }
       />
       {
         setStockInfo
-        && isStocksCodeRendered && stocksCode.map((el) => (
-          <button
-            value={ el.stockCode }
-            onClick={ saveStockInfo }
-            type="button"
-            key={ el.stockCode }
-          >
-            { el.stockCode }
-          </button>
-        ))
-      }
-    </div>
+        && stocksCode[0] && isStocksCodeRendered
+
+          && (
+          <div className="pl-6 ml-10 relative inline-block text-left">
+            <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-tr-md rounded-b-md shadow-lg bg-boleta-input-color ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+              <div className="py-1">
+                {
+                  stocksCode.map((el) => (
+                    <div>
+                      <button
+                        value={ el.stockCode }
+                        onClick={ saveStockInfo }
+                        type="button"
+                        key={ el.stockCode }
+                        className="ml-1 text-custom-white"
+                      >
+                        { el.stockCode }
+                      </button>
+                    </div>
+                  ))
+                  }
+              </div>
+            </div>
+          </div>
+          )
+        }
+    </>
   );
 }
