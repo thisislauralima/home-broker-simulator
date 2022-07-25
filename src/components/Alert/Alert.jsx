e38713b1c5442313e3eeba8f83b2797dbb46842d
Laura Lima <lauraolima2000@gmail.com>
2022-07-24 21:11:35 -0300

    feat: style alert component

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,29 +1,27 @@
 import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import stockContext from '../../context/stockContext';
-import './alert.css';
+import closeIcon from '../../images/orange-close-icon.png';
 
 export default function Alert({ message }) {
   const {
     setErrorMessage,
   } = useContext(stockContext);
 
-  const closeAlert = () => {
+  const closeError = () => {
     setErrorMessage('');
   };
 
   return (
-    <div id="alert-container">
-      <div id="all-error-messages">
-        <div>
-          <span>Erro</span>
-          <div>
-            <span>Verifique: </span>
-            <span id="error-message">{ message }</span>
-          </div>
-        </div>
-        <button onClick={ closeAlert } id="alert-btn" type="button">X</button>
+    <div className="w-full bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+      <div className="flex justify-between">
+        <p className="font-bold">Aviso</p>
+        <button className="w-5" onClick={ closeError } type="button">
+          <img src={ closeIcon } alt="close-error-msg" />
+        </button>
+
       </div>
+      <p>{ message }</p>
     </div>
   );
 }
