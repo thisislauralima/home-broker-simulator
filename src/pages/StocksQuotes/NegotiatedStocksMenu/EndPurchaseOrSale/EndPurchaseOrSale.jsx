c54445d0daeb43545e70551884520df43e92cd93
Laura Lima <lauraolima2000@gmail.com>
2022-07-24 21:22:40 -0300

    feat: EndPurchaseOrSale component - cofirms stock purchase or sale

--- src/pages/StocksQuotes/NegotiatedStocksMenu/EndPurchaseOrSale/EndPurchaseOrSale.jsx
+++ src/pages/StocksQuotes/NegotiatedStocksMenu/EndPurchaseOrSale/EndPurchaseOrSale.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import stockContext from '../../../../context/stockContext';
+import closeIcon from '../../../../images/close-icon.png';
 
 export default function EndPurchaseOrSale() {
   const {
@@ -8,9 +9,12 @@ export default function EndPurchaseOrSale() {
     stockFinalPriceDecimal,
     inputValueQuantityStock,
     setBoughtStocks,
-    boughtStocks,
     setIsPurchaseDone,
     setIsEndPurchaseOrSaleRendered,
+    setIsMainTableRendered,
+    setIsStockMenuRendered,
+    setIsAskToInvest,
+    boughtStocks,
   } = useContext(stockContext);
 
   const endPurchase = () => {
@@ -20,33 +24,63 @@ export default function EndPurchaseOrSale() {
         id: uuidv4(),
         stockId: stockInfo[0].id,
         stockName: stockInfo[0].name,
-        quantity: inputValueQuantityStock,
+        quantity: Number(inputValueQuantityStock),
         price: stockFinalPriceDecimal,
       }]);
     setIsPurchaseDone(true);
     setIsEndPurchaseOrSaleRendered(false);
+
+    window.localStorage.setItem('boughtStocks', JSON.stringify([...boughtStocks, {
+      id: uuidv4(),
+      stockId: stockInfo[0].id,
+      stockName: stockInfo[0].name,
+      quantity: Number(inputValueQuantityStock),
+      price: stockFinalPriceDecimal,
+    }]));
   };
 
-  useEffect(() => {
-    localStorage.setItem('boughtStock', JSON.stringify(boughtStocks));
-  }, [boughtStocks]);
+  const endOperation = () => {
+    setIsEndPurchaseOrSaleRendered(false);
+    setIsStockMenuRendered(false);
+    setIsMainTableRendered(true);
+    setIsAskToInvest(false);
+  };
 
   return (
-    <div>
-      <h3>FINALIZAR OPERAÇÃO</h3>
-      <p>Empresa/ Código</p>
-      {
-        stockInfo.length && stockInfo.map((el) => <div key={ el.id }>{el.name}</div>)
-      }
-      <p>Preço</p>
-      {
-        stockInfo.length && stockInfo.map((el) => <div key={ el.id }>{el.price}</div>)
-      }
-      <p>Quantidade</p>
-      <p>{ inputValueQuantityStock }</p>
-      <p>Valor investimento</p>
-      <p>{ stockFinalPriceDecimal }</p>
-      <button onClick={ endPurchase } type="button">ENVIAR</button>
-    </div>
+    <section className="bg-end-operation-gray-lighter  text-center text-end-operation-font-color-gray max-w-4xl mx-4 min-w-[300px] mt-10">
+      <div className="flex justify-end">
+        <button onClick={ endOperation } type="button">
+          <img className="w-5 h-5 cursor-pointer" src={ closeIcon } alt="close-order-btn" />
+        </button>
+      </div>
+      <h3 className="pb-4">
+        FINALIZAR OPERAÇÃO
+      </h3>
+      <div className="py-10 pb-3 max-w-4xl min-w-[300px] text-black bg-end-operation-gray-darker">
+        <div className="flex justify-center">
+          <p>Empresa/ Código:</p>
+          {
+          stockInfo.length && stockInfo.map((el) => <div className="ml-1" key={ el.id }>{`${el.name} ${el.stockCode}`}</div>)
+        }
+        </div>
+        <div className="flex justify-center">
+          <p>Preço: </p>
+          {
+          stockInfo.length && stockInfo.map((el) => <div className="ml-1" key={ el.id }>{el.price}</div>)
+        }
+        </div>
+        <div className="flex justify-center">
+          <p>Quantidade:</p>
+          <p className="ml-1">{ inputValueQuantityStock }</p>
+        </div>
+        <div className="flex justify-center">
+          <p>Valor da operação:</p>
+          <p className="ml-1">{ stockFinalPriceDecimal }</p>
+        </div>
+        <div className="py-10 pb-2 text-center">
+          <button className="p-2.5 rounded-md bg-my-custom-blue text-white" onClick={ endPurchase } type="button">ENVIAR</button>
+        </div>
+      </div>
+    </section>
   );
 }
