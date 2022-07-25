2636edb21f3665309fc47bfd1e190f5cf7f34988
Laura Lima <lauraolima2000@gmail.com>
2022-07-24 21:25:27 -0300

    feat: endOperationBtn component - validates info before selling or buying a stock

--- src/pages/StocksQuotes/NegotiatedStocksMenu/Form/EndOperationBtn/EndOperationBtn.jsx
+++ src/pages/StocksQuotes/NegotiatedStocksMenu/Form/EndOperationBtn/EndOperationBtn.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import stockContext from '../../../../../context/stockContext';
 import './endOperationBtn.css';
 
-export default function EndOperationBtn() {
+export default function EndOperationBtn({ isPersonalMenu }) {
   const {
     stockFinalPriceDecimal,
     inputValueQuantityStock,
@@ -12,6 +13,11 @@ export default function EndOperationBtn() {
     setErrorMessage,
     setIsEndPurchaseOrSaleRendered,
     setAccountBalance,
+    setIsStockMenuRendered,
+    setIsMainTableRendered,
+    btnColor,
+    setIsPersonalMenuOpened,
+    setIsPersonalTableOpened,
   } = useContext(stockContext);
 
   const validateStockCodeInput = () => {
@@ -23,13 +29,13 @@ export default function EndOperationBtn() {
   };
 
   const validateStockQuantity = () => {
-    if (inputValueQuantityStock === 0) {
+    if (Number(inputValueQuantityStock) === 0) {
       setErrorMessage('Selecione a quantidade do lote.');
       return false;
-    } if (inputValueQuantityStock % 100 !== 0) {
+    } if (Number(inputValueQuantityStock) % 100 !== 0) {
       setErrorMessage('A quantidade de ação não é múltipla de lote.');
       return false;
-    } if (inputValueQuantityStock > stockInfo[0].quantity) {
+    } if (Number(inputValueQuantityStock) > stockInfo[0].quantity) {
       setErrorMessage('A quantidade do lote inserida não pode ser superior a disponível.');
       return false;
     }
@@ -49,19 +55,97 @@ export default function EndOperationBtn() {
     return 'tudo certo';
   };
 
+  const validateQuantityAtSale = () => {
+    const allBoughtStocks = JSON.parse(localStorage.getItem('boughtStocks'));
+    const boughtQuantity = allBoughtStocks.filter((stock) => stock.quantity);
+    if (boughtQuantity > Number(inputValueQuantityStock)) {
+      setErrorMessage('Não é permitido vender uma quantia além da comprada.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
+  const checkIfStockIsBought = () => {
+    const allBoughtStocks = JSON.parse(localStorage.getItem('boughtStocks'));
+    if (!allBoughtStocks.includes(stockInfo[0].name)) {
+      setErrorMessage('O código da ação inserido não está na sua carteira.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
+  const validateQuantityInputAtSale = () => {
+    if (Number(inputValueQuantityStock) === 0) {
+      setErrorMessage('Selecione a quantidade do lote.');
+      return false;
+    } if (Number(inputValueQuantityStock) % 100 !== 0) {
+      setErrorMessage('A quantidade de ação não é múltipla de lote.');
+      return false;
+    }
+    return 'tudo certo';
+  };
+
+  const removeSoldStock = () => {
+    JSON.parse(localStorage.getItem('boughtStocks')).filter((stock) => (
+      stock.stockName !== stockInfo[0].name));
+  };
+  const validateStockSale = () => {
+    if (validateQuantityAtSale() === 'tudo certo'
+    && validateStockCodeInput() === 'tudo certo'
+    && validateQuantityInputAtSale() === 'tudo certo'
+    && checkIfStockIsBought() === 'tudo certo') {
+      setErrorMessage('');
+      setIsPersonalTableOpened(false);
+      setIsPersonalMenuOpened(false);
+      setIsEndPurchaseOrSaleRendered(true);
+      removeSoldStock();
+    }
+  };
+
+  const validatePersonalPurchase = () => {
+    if (validateStockQuantity() === 'tudo certo'
+    && validateStockCodeInput() === 'tudo certo'
+    && validateAccountBalance() === 'tudo certo') {
+      setErrorMessage('');
+      updateAccountBalance();
+      setIsPersonalTableOpened(false);
+      setIsPersonalMenuOpened(false);
+      setIsEndPurchaseOrSaleRendered(true);
+    }
+  };
+
+  const validatePersonaleTransactions = () => {
+    if (btnColor === 'my-custom-green') {
+      validateStockSale();
+      return;
+    } if (btnColor === 'my-custom-yellow') {
+      validatePersonalPurchase();
+    }
+  };
+
   const endOperation = () => {
+    if (isPersonalMenu) {
+      validatePersonaleTransactions();
+      return;
+    }
     if (validateStockCodeInput() === 'tudo certo'
       && validateStockQuantity() === 'tudo certo'
       && validateAccountBalance() === 'tudo certo') {
       setErrorMessage('');
       setIsEndPurchaseOrSaleRendered(true);
+      setIsStockMenuRendered(false);
+      setIsMainTableRendered(false);
       updateAccountBalance();
     }
   };
 
   return (
     <div id="send-order-btn-div">
-      <button onClick={ endOperation } id="send-order-btn" type="button">ENVIAR ORDEM</button>
+      <button onClick={ endOperation } id="send-order-btn" type="button">Enviar ordem</button>
     </div>
   );
 }
+
+EndOperationBtn.propTypes = {
+  isPersonalMenu: PropTypes.bool,
+}.isRequired;
