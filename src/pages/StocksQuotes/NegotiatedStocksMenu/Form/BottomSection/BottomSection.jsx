fbed55e525a1398b33f83e546d37f56e429f9165
Laura Lima <lauraolima2000@gmail.com>
2022-07-24 21:20:16 -0300

    feat: bottom section component
    shows info about the stock that is being purchased or saled

---
+++ src/pages/StocksQuotes/NegotiatedStocksMenu/Form/BottomSection/BottomSection.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import stockContext from '../../../../../context/stockContext';
+import allStocks from '../../../../../data/allStocks';
+
+export default function BottomSection() {
+  const {
+    stockInfo,
+    stockFinalPriceDecimal,
+    btnColor,
+  } = useContext(stockContext);
+
+  return (
+    <div className={ `text-boleta-form-gray flex justify-around w-full mt-3.5 pb-0 bg-${btnColor}` }>
+      {
+        stockInfo.length && allStocks.filter(
+          (stock) => stock.id === stockInfo[0].id,
+        ).map(({ price }) => <div key={ price }>{ `Valor do ativo: ${price}` }</div>)
+      }
+      <div className="text-boleta-form-gray">{ `Total: ${stockFinalPriceDecimal}` }</div>
+    </div>
+  );
+}
