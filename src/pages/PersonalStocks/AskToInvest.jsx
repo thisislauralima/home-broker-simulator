bad59754311f85f2ee10856f2004f44a5bca385c
Laura Lima <lauraolima2000@gmail.com>
2022-09-05 01:19:48 -0300

    refactor: align asktoinvest component to center of page

--- src/pages/PersonalStocks/AskToInvest.jsx
+++ src/pages/PersonalStocks/AskToInvest.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function AskToInvest() {
   return (
-    <main>
+    <main className="flex h-screen flex-col items-center justify-center">
       <p>Você ainda não possui investimentos.</p>
       <p>Que tal dar uma olhada nas nossas cotações?</p>
       <Link to="/acoes">Clique aqui para conferir!</Link>
